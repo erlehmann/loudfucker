b5e88e7a2254a0a481290eb8b9dfed38a7af316b
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2012-11-18 15:55:54 +0100

    * improved loudplotter

--- loudplotter.py
+++ loudplotter.py
@@ -5,8 +5,11 @@ from pylab import *
 from sys import stdin
 
 rdata = genfromtxt(stdin, delimiter='\t', names = ('M', 'S', 'I'))
-samples = range(len(rdata))
-M = plot(samples, rdata['M'])
-S = plot(samples, rdata['S'])
-I = plot(samples, rdata['I'])
+times = [0.4*i for i in range(len(rdata))]
+M = plot(times, rdata['M'])
+S = plot(times, rdata['S'])
+I = plot(times, rdata['I'])
+xlabel('Time (s)')
+ylabel('Loudness (LUFS)')
+ylim(-80,0)
 show()
